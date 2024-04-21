8c50ab934bdc0d2a31473fc0f31c6b88ef0b803b
Shivay Gupta <shivaygupta00@gmail.com>
2024-04-21 13:56:24 +0530

    first commit

---
+++ git-explore/src/main/java/com/example/gitexplore/GitExploreApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExploreApplication.class, args);
+	}
+
+}
---
+++ git-explore/src/test/java/com/example/gitexplore/GitExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitexplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
